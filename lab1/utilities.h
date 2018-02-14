cfb3c6b31306d1625be9eac35d332e457480dc8e
Teyhey <Teyhey@users.noreply.github.com>
2018-02-14 17:43:16 -0500

    Added the Macro Guard
    Strange name

--- lab1/utilities.h
+++ lab1/utilities.h
@@ -1,4 +1,13 @@
 
-void **malloc2D(size_t rows, size_t cols, size_t sizeOfType);
+// John Tejeda
 
+
+#ifndef Utilities_H
+#define Utilities_H
+
+
+// Function Declarations
+void **malloc2D(size_t rows, size_t cols, size_t sizeOfType);
 void free2D(void ***matrix);
+
+#endif /* Utilities_H */
