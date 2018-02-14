8fa151f59948b4fb149a46d8e3b442e4f9ec2276
Teyhey <Teyhey@users.noreply.github.com>
2018-02-14 18:11:23 -0500

    Adding stuff

--- lab1/utilities.h
+++ lab1/utilities.h
@@ -2,12 +2,12 @@
 // John Tejeda
 
 
-#ifndef Utilities_H
-#define Utilities_H
+#ifndef utilities_H
+#define utilities_H
 
 
 // Function Declarations
 void **malloc2D(size_t rows, size_t cols, size_t sizeOfType);
 void free2D(void ***matrix);
 
-#endif /* Utilities_H */
+#endif /* utilities_H */
--- lab1/vector_test.c
+++ lab1/vector_test.c
@@ -1,1 +1,21 @@
 
+// John Tejeda
+
+#include "types.h"
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+
+int main( int argc, const char* argv[] )
+{
+    unsigned int N; // Input the the user will enter in console
+
+	cout << "Number of elements ===>";
+    cin >> N;
+
+    Vector3t[] points = new Vector3t[N];
+
+}
+
+
