b93b9a1e5f76c4d06b144c7949b9a1ba36b5b96e
Teyhey <Teyhey@users.noreply.github.com>
2018-02-14 18:35:45 -0500

    Need to figure out how to split a comma string into ints

--- lab1/vector_test.c
+++ lab1/vector_test.c
@@ -1,21 +1,35 @@
-
 // John Tejeda
 
 #include "types.h"
 #include <stdio.h>
 #include <iostream>
+#include <string>
+#include <sstream>
+
 using namespace std;
 
+int main(int argc, const char* argv[]) {
+	unsigned int N; // Input the the user will enter in console.
 
-int main( int argc, const char* argv[] )
-{
-    unsigned int N; // Input the the user will enter in console
+	int trip_1;
+	int trip_2;
+	int trip_3;
+	string trips;
 
-	cout << "Number of elements ===>";
-    cin >> N;
+	cout << "Number of elements ===> ";
+	cin >> N;
 
-    Vector3t[] points = new Vector3t[N];
+Vector3t[] points = new Vector3t[N];
+
+for(int e = 0; e > points.size();, e++) {
+	cout << "Please enter 3 integers (comma separated): ";
+	cin >> trips;
+
+	stringstream split_Ints(trips);
+
+
 
 }
 
+}
 
