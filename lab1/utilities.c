225f68e2946bcdb5a8f81c704191708ca3144f20
Teyhey <Teyhey@users.noreply.github.com>
2018-02-14 17:22:07 -0500

    Starting to work on the lab :)

--- lab1/utilities.c
+++ lab1/utilities.c
@@ -1,1 +1,21 @@
 
+
+#include "utilities.h"
+
+void **malloc2D(size_t rows, size_t cols, size_t sizeOfType)
+{
+    void *block = malloc(sizeOfType * rows * cols);
+    void **matrix = malloc(sizeof(void *) * rows);
+    for (int row = 0; row < rows; ++row)
+    {
+        matrix[row] = block + cols * row * sizeOfType;
+    }
+    return matrix;
+}
+
+void free2D(void ***matrix)
+{
+    free((*matrix)[0]);
+    free((*matrix));
+    matrix = NULL;
+}
--- lab1/utilities.h
+++ lab1/utilities.h
@@ -1,1 +1,4 @@
 
+void **malloc2D(size_t rows, size_t cols, size_t sizeOfType);
+
+void free2D(void ***matrix);
