457620b8c13a491b8d4c47d3c6525868bad176da
Teyhey <Teyhey@users.noreply.github.com>
2018-02-14 19:05:26 -0500

    Should work (kinda)

--- lab1/types.h
+++ lab1/types.h
@@ -1,18 +1,15 @@
-
 // John Tejeda
-
 
 #ifndef types_H
 #define types_H
 
-
-struct vec3{
-int x; // an int
-int y; // an int
-int z; // an int
-}; // vec3 Struct
+struct vec3 {
+	int x; // an int
+	int y; // an int
+	int z; // an int
+};
+// vec3 Struct
 
 typedef struct Vector3t vec3; // typedef for vec3
-
 
 #endif /* types_H */
--- lab1/vector_test.c
+++ lab1/vector_test.c
@@ -3,6 +3,7 @@
 #include "types.h"
 #include <stdio.h>
 #include <iostream>
+#include <vector>
 #include <string>
 #include <sstream>
 
@@ -10,7 +11,8 @@ using namespace std;
 
 int main(int argc, const char* argv[]) {
 	unsigned int N; // Input the the user will enter in console.
-
+	vector<int> vect;
+	int i;
 	int trip_1;
 	int trip_2;
 	int trip_3;
@@ -21,15 +23,27 @@ int main(int argc, const char* argv[]) {
 
 Vector3t[] points = new Vector3t[N];
 
-for(int e = 0; e > points.size();, e++) {
+for(int e = 0; e < points.size();, e++) {
 	cout << "Please enter 3 integers (comma separated): ";
 	cin >> trips;
 
-	stringstream split_Ints(trips);
+	stringstream split_Ints(trips); // Splits the String up into little bits.
 
+	while (ss >> i)
+	{
+		vect.push_back(i);
 
+		if (ss.peek() == ',' || ss.peek() == ' ')
+		ss.ignore();
+	}
 
-}
+	for (int i = 0; i < vect.size(); i++)
+	cout << vect.at(i) << endl;
 
-}
+	points[e].x = vect.at(0);
+	points[e].y = vect.at(1);
+	points[e].z = vect.at(2);
+} // for
+
+} // main
 
